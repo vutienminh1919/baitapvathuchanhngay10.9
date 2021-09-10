d34e07e222b858e7c18fd80bb909bf9763982250
VuTienMinh1919 <vutienminh1919@gmail.com>
2021-09-10 11:02:57 +0700

    nop bai thuc hanh 2

---
+++ thuchanhsudungtoantu.js
@@ -0,0 +1,6 @@
+let a = prompt('nhap chieu dai');
+let b = prompt('nhap chieu rong');
+let chieudai = parseInt(a);
+let chieurong = parseInt(b);
+let dientich = chieudai*chieurong;
+alert('dien tich la '+dientich);
